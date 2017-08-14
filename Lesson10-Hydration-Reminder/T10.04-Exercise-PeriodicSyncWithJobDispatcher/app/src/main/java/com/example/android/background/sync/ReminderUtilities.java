e2bfd391bcaa9f538365123efc5faedddec23a14
Dmitry <dmitry.drobotov123@gmail.com>
2017-08-15 00:23:01 +0300

    Complete T10.04

--- Lesson10-Hydration-Reminder/T10.04-Exercise-PeriodicSyncWithJobDispatcher/app/src/main/java/com/example/android/background/sync/ReminderUtilities.java
+++ Lesson10-Hydration-Reminder/T10.04-Exercise-PeriodicSyncWithJobDispatcher/app/src/main/java/com/example/android/background/sync/ReminderUtilities.java
@@ -35,7 +35,7 @@ public class ReminderUtilities {
     //  - REMINDER_JOB_TAG should be a String constant, storing something like "hydration_reminder_tag"
     //  - sInitialized should be a private static boolean variable which will store whether the job
     //    has been activated or not
-    private static final int REMINDER_INTERVAL_MINUTES = 15;
+    private static final int REMINDER_INTERVAL_MINUTES = 1;
     private static final int REMINDER_INTERVAL_SECONDS = (int) TimeUnit.MINUTES.toSeconds(REMINDER_INTERVAL_MINUTES);
     private static final int SYNC_FLEXIBLE_SECONDS = REMINDER_INTERVAL_SECONDS;
     private static final String REMINDER_JOB_TAG = "hydration_reminder_tag";
